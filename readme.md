fe01dce7a803a007c6682cc7ab624c799a6538e3
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-01 16:27:44 +0900

    completed

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+Thanks to this [YouTube](https://youtu.be/nSXnif14lVo)
+
 1. ~node.jsをインストールする。~
     - `node -v`
     - npmも使えるか確認`npm -v`
@@ -6,3 +8,6 @@
 3. `node ファイル名`
 4. モジュールをインストールしたり？
     - `npm install --save-dev http`
+5. サーバをいちいち落とさなくてもリロードされる。
+    - `npm install -g nodemon`
+    - `server.js`で保存をする。
--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("nodejs")
+const http = require("http");
+const PORT = 8000;
+const html = require("fs").readFileSync("./index.html");
+
+const server = http.createServer((req, res) => {
+    //ブラウザからアクセスが来た時の処理
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(html);
+    res.end();
+
+    if(req.method == "GET") {
+    }
+    
+    if(req.method == "POST") {
+    }
+});
+
+server.listen(PORT, () => {
+    console.log("server running!");
+});
